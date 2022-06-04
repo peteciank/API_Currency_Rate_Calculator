66e6aac91ffd51873b9d7519aa91146fb108180c
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-04 18:48:51 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
-st.markdown("Hello George. This is Peter.")
+st.markdown("Version 1. Bringing data from API")
 
 import pandas as pd
 import requests
 
+# Source of APIs https://github.com/Castrogiovanni20/api-dolar-argentina
+
+# Code for historical evolution of Dollar in Argentina
 #url = 'https://api-dolar-argentina.herokuapp.com/api/evolucion/dolaroficial'
 #r = requests.get(url)
 #json = r.json()
