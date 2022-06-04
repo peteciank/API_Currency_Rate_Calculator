1704aa9d3c55642e4ba2a762241109d4f4ea9973
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-04 16:15:18 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.markdown("Hello World. This is Peter.")
+
+import pandas as pd
+import requests
+
+url = 'https://api-dolar-argentina.herokuapp.com/api/evolucion/dolaroficial'
+r = requests.get(url)
+json = r.json()
+df = pd.DataFrame(json['meses'])
+
+
+st.markdown(df)
