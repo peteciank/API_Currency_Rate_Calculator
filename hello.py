2372f3bdd16ec1d3216bd82b22db002d0f275019
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-04 16:24:38 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,10 +5,14 @@ st.markdown("Hello George. This is Peter.")
 import pandas as pd
 import requests
 
-url = 'https://api-dolar-argentina.herokuapp.com/api/evolucion/dolaroficial'
+#url = 'https://api-dolar-argentina.herokuapp.com/api/evolucion/dolaroficial'
+#r = requests.get(url)
+#json = r.json()
+#df = pd.DataFrame(json['meses'])
+
+url = 'https://api-dolar-argentina.herokuapp.com/api/dolaroficial'
 r = requests.get(url)
 json = r.json()
-df = pd.DataFrame(json['meses'])
-
+df = pd.DataFrame(json, index=[0])
 
 st.markdown(df)
