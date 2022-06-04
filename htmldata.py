10b15f6799c8524fdb5b39cbb01ac4b009c76ba2
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-04 10:07:19 -0300

    Update htmldata.py

--- htmldata.py
+++ htmldata.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import streamlit as st
+import base64
+import matplotlib.pyplot as plt
+import numpy as np
+import yfinance as yf
 
 df = pd.read_html('https://en.wikipedia.org/wiki/Poverty')
 df[1]
