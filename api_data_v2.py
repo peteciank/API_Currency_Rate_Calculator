80d24f67cd43914290f33a25f1abbb9dd0a301c7
peteciank <106826936+peteciank@users.noreply.github.com>
2022-06-05 10:20:02 -0300

    Update api_data_v2.py

--- api_data_v2.py
+++ api_data_v2.py
@@ -5,31 +5,27 @@ st.markdown("Version 2. Bringing data from API")
 import pandas as pd
 import requests
 
-# This was part of version 1
-# url = 'https://api-dolar-argentina.herokuapp.com/api/dolaroficial'
-# r = requests.get(url)
-# json = r.json()
-# df = pd.DataFrame(json, index=[0])
-
-new_url = st.text_input('Enter URL API with Anonymous Access')
-
-
-#st.button("Show API Data", on_click=load_data(new_url))
+# new_url = st.text_input('Enter URL API with Anonymous Access')
+# url='https://api-dolar-argentina.herokuapp.com/api/dolaroficial'
 
 selection = st.selectbox('Seleccionar Tipo de Cambio', ["Dolar Oficial","Dolar Blue","Dolar Bolsa"])
 st.write(selection)
 
-
-#@st.cache
-def load_data(url='https://api-dolar-argentina.herokuapp.com/api/dolaroficial'):
-    # url = 'https://api-dolar-argentina.herokuapp.com/api/dolaroficial'
+def load_data(option):
+    if option = "Dolar Oficial":
+       url = 'https://api-dolar-argentina.herokuapp.com/api/dolaroficial'
+    elif option = "Dolar Blue":
+       url = 'https://api-dolar-argentina.herokuapp.com/api/dolarblue'
+    else:
+       url = 'https://api-dolar-argentina.herokuapp.com/api/dolarbolsa'
+    
     r = requests.get(url)
     json = r.json()
     df = pd.DataFrame(json, index=[0])
     st.dataframe(df)
-    #return df
+
 
 #def ShowData():
 #    st.dataframe(load_data(url))
 
-load_data(new_url)
+load_data(selection)
